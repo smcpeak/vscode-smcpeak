1f9f51ee90d1782e76ca527f07d4f55af449039b
Scott McPeak <scott.g.mcpeak@gmail.com>
2019-08-19 21:35:28 -0700

    Replace hello-world command with insertDateTime.

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Return 'n' as a string, padded to two digits.
+// It works for integer 'n' in [0,99].
+function dd(n: number) : string {
+  if (n < 10) {
+    return "0" + n;
+  }
+  else {
+    return "" + n;
+  }
+}
+
+// Return the current date/time as "YYYY-MM-DD HH:MM".
+function currentDateTime() : string {
+  let d = new Date();
+  return "" + d.getFullYear() +
+         "-" + dd(d.getMonth()+1) +
+         "-" + dd(d.getDate()) +
+         " " + dd(d.getHours()) +
+         ":" + dd(d.getMinutes());
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-smcpeak" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
-
+  let disposable = vscode.commands.registerTextEditorCommand('extension.insertDateTime',
+    (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+      edit.insert(textEditor.selection.active, currentDateTime());
+    });
   context.subscriptions.push(disposable);
 }
 
